3cb2855b8456c10bd30d83a0562cda752b6bcc5a
Dan Atanasov <danmanm@gmail.com>
2026-08-16 18:13:20 +0300

    Add top-level page navigation to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
-export default function Home() {
+import Link from "next/link";
+
+import { getTopLevelPages } from "@/app/utils/getTopLevelPages";
+
+export default async function Home() {
+  const pages = await getTopLevelPages();
+
   return (
-    <div className="h-dvh flex flex-col justify-around">
+    <div className="h-dvh flex flex-col items-center text-center justify-around">
       <h1 className="text-6xl">
         Docs for <span className="text-primary">casual</span> players
       </h1>
 
       <p>Games can be fun. They can be hard. They can be both.</p>
+
+      <nav aria-labelledby="game-pages-heading">
+        <ul className="list list-none p-0">
+          {pages.map((page) => (
+            <li key={page.href}>
+              <Link href={page.href}>{page.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
